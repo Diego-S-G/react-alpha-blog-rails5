20eb01e950fc4e155382620dc57ce5ac8d0adc01
Diego-S-G <diego07goncalves@gmail.com>
2024-12-26 15:29:24 -0300

    Creating the ColorPicker Constructor.

--- app/javascript/components/ColorPicker.js
+++ app/javascript/components/ColorPicker.js
@@ -3,6 +3,12 @@ import PropTypes from "prop-types";
 import { SketchPicker } from "react-color";
 
 class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = {};
+
   render() {
     return (
       <React.Fragment>
