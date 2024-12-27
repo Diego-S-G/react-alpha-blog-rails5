b4dc44c63e2c847013ad0371f4047009b9e80618
Diego-S-G <diego07goncalves@gmail.com>
2024-12-27 09:11:43 -0300

    Wiring the component to the form field.

--- app/javascript/components/ColorPicker.js
+++ app/javascript/components/ColorPicker.js
@@ -6,11 +6,16 @@ class ColorPicker extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { selector: props.selector };
+
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = (color) => {
     document.body.style.background = color.hex;
+
+    let elem = document.querySelector('#'+this.state.selector);
+    elem.value = color.hex
   }
 
   render() {
